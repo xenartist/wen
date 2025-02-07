a2622dc09813e79566d4784d5484e387d4333738
xenartist <146194585+xenartist@users.noreply.github.com>
2025-02-07 18:49:57 +1100

    remove config file logic

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,6 @@ fn menu_selected(siv: &mut Cursive, item: &str) {
 }
 
 fn main() {
-    // Initialize ledger configuration
-    ledger::setup_ledger();
-
     // Initialize the cursive interface
     let mut siv = cursive::default();
     
